6416eec7c065fe75705bf203d5991cd2598930da
John <john.orr10@outlook.com>
2025-07-24 13:11:06 +0100

    2 waves of diffferent frequencies

--- ft.py
+++ ft.py
@@ -2,18 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-repetions = 10
-t = np.linspace(0, 2 * repetions * np.pi, 1000)
+duration = 5
+samples = 1000
 
-wave1 = np.sin(t)
-wave2 = np.sin(t + np.pi/2)
+t = np.linspace(0, duration, 1000)
 
+wave1 = np.sin(2 * np.pi * t * 3)
+wave2 = np.sin(2 * np.pi * t * 2)
+
+plt.figure(figsize=(10, 6))
+
+plt.subplot(2, 1, 1)
 plt.plot(t, wave1)
 plt.plot(t, wave2)
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
+plt.ylim(-3,3)
+
+
+plt.subplot(2, 1, 2)
 plt.plot(t, wave1 + wave2)
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
-# plt.xlim(2 * repetions * np.pi)
 plt.ylim(-3,3)
+
+plt.tight_layout()
 plt.show()
