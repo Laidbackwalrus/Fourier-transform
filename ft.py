f1ae1b1fdb322511db2f946bbd2da61ede48141f
John <john.orr10@outlook.com>
2025-07-24 13:06:15 +0100

    plotting 2 waves + their sum

--- ft.py
+++ ft.py
@@ -6,10 +6,14 @@ repetions = 10
 t = np.linspace(0, 2 * repetions * np.pi, 1000)
 
 wave1 = np.sin(t)
+wave2 = np.sin(t + np.pi/2)
 
 plt.plot(t, wave1)
+plt.plot(t, wave2)
+plt.plot(t, wave1 + wave2)
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
+plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
 # plt.xlim(2 * repetions * np.pi)
-plt.ylim(-2,2)
+plt.ylim(-3,3)
 plt.show()
